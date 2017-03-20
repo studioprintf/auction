0b1fba9df21b89e6e00bda87249a14cf3ddc30ae
DTLuc <DTLuc@DESKTOP-EFDL0HO.lan>
2017-03-20 22:07:10 +0800

    修改密码action与校验器

---
+++ src/action/UserUpdatePasswordAction.java
@@ -0,0 +1,106 @@
+package action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.Action;
+
+import pojo.User;
+import service.UserManager;
+
+/** 
+ * @author Lucifer 
+ * @date 2017年3月20日 下午9:39:22 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+@Controller
+public class UserUpdatePasswordAction implements Action,SessionAware {
+	private String oldPassword;
+	private String newPassword;
+	private String confirmPassword;
+	private User user;
+	private Map<String, Object> session;
+	private UserManager userManager;
+	
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public UserManager getUserManager() {
+		return userManager;
+	}
+
+
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
+
+
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		int user_id = (int) session.get("USER_ID");
+		if(user_id==0){
+			return ERROR;
+		}
+		if(newPassword.equals(confirmPassword)&&!oldPassword.equals(newPassword)){
+			user.setUser_id(user_id);
+			user.setUser_password(newPassword);
+			if(userManager.updateUserPW(user)){
+				return SUCCESS;
+			}
+			else{
+				return ERROR;
+			}
+		}
+		return ERROR;
+	}
+
+
+}
